94b7c15dae2d9ec4cac6a2d8e875e821c97c1e0e
nericode <alanneri10@gmail.com>
2021-06-08 18:08:00 -0500

    add card component

---
+++ src/shared/components/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {StyleSheet, TouchableOpacity, Platform} from 'react-native';
+
+export const Card = props => {
+  return (
+    <TouchableOpacity
+      {...props}
+      activeOpacity={!props.activeOpacity ? 0.6 : props.activeOpacity}
+      onPress={() => {
+        if (props.onPress) {
+          props.onPress();
+        }
+      }}
+      style={[styles.card, props.style]}>
+      {props.children}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+    elevation: 10,
+    shadowColor:
+      Platform.OS === 'android'
+        ? 'rgba(43, 47, 48, 0.8)'
+        : 'rgba(43, 47, 48, 0.1)',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 1,
+  },
+});
