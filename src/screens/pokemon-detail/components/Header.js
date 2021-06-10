87fb07018bb28dfcce99340cf9c834546e7a5abb
nericode <alanneri10@gmail.com>
2021-06-10 18:21:52 -0500

    add pokemon detail

---
+++ src/screens/pokemon-detail/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {ImageBackground, Text, Image} from 'react-native';
+import Colors from '../../../styles/Colors';
+
+function Header({pokemon}) {
+  return (
+    <ImageBackground
+      blurRadius={10}
+      style={{
+        height: 250,
+        width: '100%',
+        alignSelf: 'center',
+        borderBottomRightRadius: 40,
+        borderBottomLeftRadius: 40,
+      }}
+      source={{
+        uri: pokemon['sprites']['other']['official-artwork']['front_default'],
+      }}>
+      <Image
+        resizeMode="center"
+        style={{
+          height: 180,
+          width: 180,
+          alignSelf: 'center',
+        }}
+        source={{
+          uri: pokemon['sprites']['other']['official-artwork']['front_default'],
+        }}
+      />
+      <Text
+        style={{
+          fontSize: 26,
+          textAlign: 'center',
+          fontWeight: 'bold',
+        }}>
+        <Text style={{fontSize: 45}}>
+          {String(pokemon.name).charAt(0).toLocaleUpperCase()}
+        </Text>
+        {String(pokemon.name).substr(1, pokemon.name.length)}
+      </Text>
+    </ImageBackground>
+  );
+}
+
+export default Header;
