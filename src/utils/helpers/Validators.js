cba046f63b7b358f6e7f254ee74eb44ef99a1306
nericode <alanneri10@gmail.com>
2021-06-11 13:39:09 -0500

    fix valid url

--- src/utils/helpers/Validators.js
+++ src/utils/helpers/Validators.js
@@ -1,12 +1,7 @@
 function isValidURL(currentURL) {
-  let url;
-  try {
-    url = new URL(currentURL);
-  } catch (error) {
-    return false;
-  }
-
-  return url.protocol === 'http:' || url.protocol === 'https:';
+  const re =
+    /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  return re.test(String(currentURL).toLowerCase());
 }
 
 export {isValidURL};
