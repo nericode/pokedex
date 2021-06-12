01cc98ede5ffedca86c2b7a68b79880a565fe675
nericode <alanneri10@gmail.com>
2021-06-12 09:49:16 -0500

    add icon & splash screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './navigators/Root.navigator';
 import {navigationRef} from './navigators/Navigate';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer ref={navigationRef}>
       <RootNavigator />
